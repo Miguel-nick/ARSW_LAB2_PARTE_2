5da22879f4cef7de157c52c93696c5be65f90297
Miguel Angel Sandoval <pipiestradaesm@gmail.com>
2026-08-17 17:53:31 -0500

    I changed the code for CPU optimization

--- src/main/java/edu/eci/arst/concprg/prodcons/Consumer.java
+++ src/main/java/edu/eci/arst/concprg/prodcons/Consumer.java
@@ -5,30 +5,30 @@
  */
 package edu.eci.arst.concprg.prodcons;
 
-import java.util.Queue;
+import java.util.concurrent.BlockingQueue;
 
 /**
  *
- * @author hcadavid
+ * @author Miguel Sandoval
  */
-public class Consumer extends Thread{
-    
-    private Queue<Integer> queue;
-    
-    
-    public Consumer(Queue<Integer> queue){
-        this.queue=queue;        
+public class Consumer extends Thread {
+
+    private BlockingQueue<Integer> queue;
+
+    public Consumer(BlockingQueue<Integer> queue) {
+        this.queue = queue;
     }
-    
+
     @Override
     public void run() {
         while (true) {
-
-            if (queue.size() > 0) {
-                int elem=queue.poll();
-                System.out.println("Consumer consumes "+elem);                                
+            try {
+                int elem = queue.take();
+                System.out.println("Consumer consumes " + elem);
+            } catch (InterruptedException ex) {
+                Thread.currentThread().interrupt();
+                break;
             }
-            
         }
     }
 }
